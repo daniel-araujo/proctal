e99f9782e5626ba6402e2991c7a5695f54e9962f
Daniel Araujo <contact@daniel-araujo.pt>
2016-10-15 18:51:12 +0100

    Add command argument parse code generator

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+
 #include "proctal.h"
+#include "cmdline.h"
 
 int main(int argc, char **argv)
 {
+	struct gengetopt_args_info ai;
+
+	if (cmdline_parser(argc, argv, &ai) != 0) {
+		return 1;
+	}
+
 	return 0;
 }
