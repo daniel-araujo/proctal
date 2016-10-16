4a4e1a77ef73a4a567e28e08142a642dfee3c78b
Daniel Araujo <contact@daniel-araujo.pt>
2016-10-16 17:16:18 +0100

    Replace gengetopt with yuck
    Gengetopt does not support sub commands.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
 #include "proctal.h"
-#include "cmdline.h"
+#include "args.yucc"
 
 int main(int argc, char **argv)
 {
-	struct gengetopt_args_info ai;
+ 	yuck_t argp;
 
-	if (cmdline_parser(argc, argv, &ai) != 0) {
+	if (yuck_parse(&argp, argc, argv) != 0) {
 		return 1;
 	}
+
+	// These flags are special in that they disrupt the normal flow of
+	// execution.
+	if (argp.help_flag) {
+		yuck_auto_help(&argp);
+		goto exit;
+	} else if (argp.version_flag) {
+		yuck_auto_version(&argp);
+		goto exit;
+	}
+
+	if (argp.cmd == YUCK_NOCMD) {
+		yuck_auto_usage(&argp);
+		yuck_auto_help(&argp);
+	} else if (argp.cmd == PROCTAL_CMD_WRITE) {
+	} else if (argp.cmd == PROCTAL_CMD_READ) {
+	}
+
+exit:
+	yuck_free(&argp);
 
 	return 0;
 }
