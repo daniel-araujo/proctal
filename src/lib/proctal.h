fff34737ae82f3fa137677dad122bc897426e1ff
Daniel Araujo <contact@daniel-araujo.pt>
2016-12-21 23:01:24 +0000

    Improve documentation of some internal functions

--- src/lib/proctal.h
+++ src/lib/proctal.h
@@ -22,12 +22,16 @@ extern struct proctal_global {
 
 /*
  * Base structure of an instance.
+ *
+ * Platform agnostic functions work with this structure.
  */
 struct proctal {
-	// Memory allocator and deallocator with the same signatures (and
-	// names) as the standard malloc and free functions which are also used
-	// by default.
+	// Memory allocator with the same signature as the malloc function from
+	// the C standard library.
 	void *(*malloc)(size_t);
+
+	// Memory deallocator with the same signature as the free function from
+	// the C standard library.
 	void (*free)(void *);
 
 	// Keeps track of the last error that was set.
@@ -43,8 +47,8 @@ struct proctal {
 		// Address alignment.
 		size_t align;
 
-		// Size of the value of the address. We only want to return addresses
-		// that when dereferenced can return values of up to this size.
+		// Size of the value of the address. We only want to return
+		// addresses can be dereferenced up to this size.
 		size_t size;
 
 		// Whether to iterate over readable addresses.
@@ -103,18 +107,28 @@ void proctal_init(struct proctal *p);
 void proctal_deinit(struct proctal *p);
 
 /*
- * Sets error of an instance.
+ * Sets the error code of an instance.
+ *
+ * This is used to tell the library user what made a function fail.
  *
  * The error parameter must be a value available as a macro with the name
- * PROCTAL_ERROR as a prefix, otherwise library users won't have a way to know
- * what the error is.
+ * PROCTAL_ERROR as a prefix, otherwise library users won't have a way to check
+ * what specific error proctal_error is returning.
  */
 void proctal_set_error(proctal p, int error);
 
 /*
- * Allocates and deallocates memory for internal data structures.
+ * Allocates memory.
+ *
+ * Meant for internal data structures of an instance.
  */
 void *proctal_malloc(proctal p, size_t size);
+
+/*
+ * Deallocates memory allocated with proctal_malloc.
+ *
+ * Meant for internal data structures of an instance.
+ */
 void proctal_free(proctal p, void *addr);
 
 /*
@@ -136,6 +150,10 @@ inline void proctal_global_free(void *addr)
 	proctal_global.free(addr);
 }
 
+/*
+ * Helpful function for finding the next suitably aligned address relative to
+ * the given one. Will return the given address if it's already aligned.
+ */
 inline void *proctal_align_addr(void *addr, size_t align)
 {
 	ptrdiff_t offset = ((unsigned long) addr % align);
