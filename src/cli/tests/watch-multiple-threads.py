e994c358a31e3bb2c8660f978821419ff2fbeeaa
Daniel Araujo <contact@daniel-araujo.pt>
2017-07-10 22:47:09 +0100

    Fix watch-multiple-threads test case

--- src/cli/tests/watch-multiple-threads.py
+++ src/cli/tests/watch-multiple-threads.py
@@ -9,20 +9,35 @@ import time
 test_program = "./tests/cli/program/read-mem-mt"
 
 guinea = subprocess.Popen([test_program], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-address = guinea.stdout.readline()
-address = str(address).strip()
+address = guinea.stdout.readline().strip().decode("utf-8")
 
 watcher = subprocess.Popen(["./proctal", "watch", "--pid=" + str(guinea.pid), "--address=" + address, "-rw"], stdout=subprocess.PIPE)
+
+# Waiting for the watch command to perform.
+# TODO: Find a reliable way to detect when the watcher has started.
+time.sleep(0.033)
+
+watcher.poll()
+
+if watcher.returncode != None:
+    # With no output detected, we expect that an error message was printed
+    # instead.
+    guinea.kill()
+    guinea.wait()
+    exit(1)
 
 poll = select.poll()
 poll.register(watcher.stdout, select.POLLIN)
 
-if not poll.poll(33):
-    sys.stderr.write("Was unable to watch for memory read access in a thread other than the main one.\n")
+if not poll.poll(100):
+    sys.stderr.write("Was unable to watch for reads in a thread that is not the main one.\n")
     watcher.kill()
+    watcher.wait()
     guinea.kill()
+    guinea.wait()
     exit(1)
 
 watcher.kill()
+watcher.wait()
 guinea.kill()
+guinea.wait()
