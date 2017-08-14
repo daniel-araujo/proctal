89cf388be778adc71126bd33ccd27f3e3a02a56b
Daniel Araujo <contact@daniel-araujo.pt>
2017-08-14 15:00:29 +0100

    Move x86-64 specific code out of hijack-main.c

--- src/api/linux/execute/hijack-main.c
+++ src/api/linux/execute/hijack-main.c
@@ -24,82 +24,29 @@ extern const char proctal_linux_execute_implementation_trap_code[];
  */
 extern const size_t proctal_linux_execute_implementation_trap_code_size;
 
-#define RED_ZONE_SIZE 128
+/*
+ * Saves the state of the given task.
+ *
+ * On success it returns a handle to a savestate. On failure it returns NULL.
+ */
+void *proctal_linux_execute_implementation_save_state(struct proctal_linux *pl, pid_t tid);
 
-struct state {
-	unsigned long long rax;
-	unsigned long long rbx;
-	unsigned long long rcx;
-	unsigned long long rdx;
-	unsigned long long rsi;
-	unsigned long long rdi;
-	unsigned long long rbp;
-	unsigned long long rsp;
-	unsigned long long rip;
-	unsigned long long rflags;
-	unsigned long long r8;
-	unsigned long long r9;
-	unsigned long long r10;
-	unsigned long long r11;
-	unsigned long long r12;
-	unsigned long long r13;
-	unsigned long long r14;
-	unsigned long long r15;
+/*
+ * Restores the state of the given task from a savestate.
+ *
+ * Returns 1 on success and 0 on failure.
+ */
+int proctal_linux_execute_implementation_load_state(struct proctal_linux *pl, pid_t tid, void *state);
 
-	// TODO: Save remaining registers.
-};
+/*
+ * Creates stack frame.
+ */
+int proctal_linux_execute_implementation_create_stack_frame(struct proctal_linux *pl, pid_t tid);
 
-static inline int save_state(struct proctal_linux *pl, pid_t tid, struct state *s)
-{
-	if (!proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RAX, &s->rax)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBX, &s->rbx)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RCX, &s->rcx)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RDX, &s->rdx)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSI, &s->rsi)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RDI, &s->rdi)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBP, &s->rbp)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSP, &s->rsp)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RIP, &s->rip)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RFLAGS, &s->rflags)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R8, &s->r8)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R9, &s->r9)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R10, &s->r10)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R11, &s->r11)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R12, &s->r12)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R13, &s->r13)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R14, &s->r14)
-		|| !proctal_linux_ptrace_register(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R15, &s->r15)) {
-		return 0;
-	}
-
-	return 1;
-}
-
-static inline int load_state(struct proctal_linux *pl, pid_t tid, struct state *s)
-{
-	if (!proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RAX, &s->rax)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBX, &s->rbx)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RCX, &s->rcx)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RDX, &s->rdx)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSI, &s->rsi)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RDI, &s->rdi)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBP, &s->rbp)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSP, &s->rsp)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RIP, &s->rip)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RFLAGS, &s->rflags)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R8, &s->r8)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R9, &s->r9)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R10, &s->r10)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R11, &s->r11)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R12, &s->r12)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R13, &s->r13)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R14, &s->r14)
-		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_R15, &s->r15)) {
-		return 0;
-	}
-
-	return 1;
-}
+/*
+ * Destroys stack frame.
+ */
+int proctal_linux_execute_implementation_destroy_stack_frame(struct proctal_linux *pl, pid_t tid);
 
 int proctal_linux_execute_implementation(
 	struct proctal_linux *pl,
@@ -108,10 +55,18 @@ int proctal_linux_execute_implementation(
 {
 	int ret = 0;
 
-	struct state savestate;
-
 	if (!proctal_linux_ptrace_attach(pl)) {
 		goto exit0;
+	}
+
+	void *savestate = proctal_linux_execute_implementation_save_state(pl, pl->pid);
+
+	if (savestate == NULL) {
+		goto exit1;
+	}
+
+	if (!proctal_linux_execute_implementation_create_stack_frame(pl, pl->pid)) {
+		goto exit2;
 	}
 
 	void *payload_location = proctal_linux_allocate(
@@ -120,7 +75,7 @@ int proctal_linux_execute_implementation(
 		PROCTAL_ALLOCATE_PERMISSION_WRITE | PROCTAL_ALLOCATE_PERMISSION_EXECUTE | PROCTAL_ALLOCATE_PERMISSION_READ);
 
 	if (payload_location == NULL) {
-		goto exit1;
+		goto exit3;
 	}
 
 	void *no_op_code_location = payload_location;
@@ -129,40 +84,31 @@ int proctal_linux_execute_implementation(
 
 	void *landing_zone = (char *) no_op_code_location + (proctal_linux_execute_implementation_no_op_code_size / 2);
 
-	if (!save_state(pl, pl->pid, &savestate)) {
-		goto exit1;
-	}
-
-	unsigned long long stack_pointer = savestate.rsp - RED_ZONE_SIZE;
-	unsigned long long base_pointer = stack_pointer;
-
-	// Create new stack frame.
-	if (!proctal_linux_ptrace_register_set(pl, pl->pid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSP, &stack_pointer)
-		|| !proctal_linux_ptrace_register_set(pl, pl->pid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBP, &base_pointer)) {
-		goto exit3;
-	}
-
 	// Place payload.
 	if (!proctal_linux_mem_write(pl, no_op_code_location, proctal_linux_execute_implementation_no_op_code, proctal_linux_execute_implementation_no_op_code_size)
 		|| !proctal_linux_mem_write(pl, bytecode_location, bytecode, bytecode_length)
 		|| !proctal_linux_mem_write(pl, trap_code_location, proctal_linux_execute_implementation_trap_code, proctal_linux_execute_implementation_trap_code_size)) {
-		goto exit3;
+		goto exit4;
 	}
 
 	// Execute payload. Will block until the trap signal is received.
 	if (!proctal_linux_ptrace_instruction_pointer_set(pl, pl->pid, landing_zone)
 		|| !proctal_linux_ptrace_cont(pl, pl->pid)
 		|| !proctal_linux_ptrace_wait_trap(pl, pl->pid)) {
-		goto exit3;
+		goto exit4;
 	}
 
 	ret = 1;
+exit4:
+	proctal_linux_deallocate(pl, payload_location);
 exit3:
-	if (!load_state(pl, pl->pid, &savestate)) {
+	if (!proctal_linux_execute_implementation_destroy_stack_frame(pl, pl->pid)) {
 		ret = 0;
 	}
 exit2:
-	proctal_linux_deallocate(pl, payload_location);
+	if (!proctal_linux_execute_implementation_load_state(pl, pl->pid, savestate)) {
+		ret = 0;
+	}
 exit1:
 	if (!proctal_linux_ptrace_detach(pl)) {
 		ret = 0;
---
+++ src/api/linux/execute/x86_64/stackframe.c
@@ -0,0 +1,31 @@
+#include "api/linux/proctal.h"
+#include "api/linux/ptrace.h"
+
+#define RED_ZONE_SIZE 128
+
+int proctal_linux_execute_implementation_create_stack_frame(struct proctal_linux *pl, pid_t tid)
+{
+	unsigned long long stack_pointer;
+	unsigned long long base_pointer;
+
+	proctal_linux_ptrace_register(
+		pl,
+		tid,
+		PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSP,
+		&stack_pointer);
+
+	stack_pointer -= RED_ZONE_SIZE;
+	base_pointer = stack_pointer;
+
+	if (!proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RSP, &stack_pointer)
+		|| !proctal_linux_ptrace_register_set(pl, tid, PROCTAL_LINUX_PTRACE_REGISTER_X86_64_RBP, &base_pointer)) {
+		return 0;
+	}
+
+	return 1;
+}
+
+int proctal_linux_execute_implementation_destroy_stack_frame(struct proctal_linux *pl, pid_t tid)
+{
+	return 1;
+}
